dad617dbcc826da009b56f77d45bd2af50fbc689
LaPatateImpériale <Noah.piller@studentfr.ch>
2025-05-21 10:12:14 +0200

    eye to buzzer

--- main.c
+++ main.c
@@ -38,7 +38,20 @@ int main(void)
         distance = ultrason_distance();
         DELAY_milliseconds(200);
         abc=distance;
+        
+         if (distance <= 10){
+            buzzer_tone(1308, 50,0);
+            
+         } else if (distance <= 15){ 
+                buzzer_sing(SING_OHOOH);
+               
+         } else if (distance <= 20) {
+                buzzer_sing(SING_HAPPY);
+             }else{
+                buzzer_tone(2936, 50, 0);
+        
     }
+  }
 }
 //----------------------------------------------------------
 // FIN DU CODE
